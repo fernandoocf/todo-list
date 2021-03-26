897248723ea46ce985a60739d1058beedec35e6e
Fernando Oliveira <fernandoocf22@gmail.com>
2021-03-26 20:18:26 -0300

    adding redux

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoInput() {
+    return (
+        <div>
+            <div className='row'>
+                <input type="text" className="col form-control"/>
+                <button className="btn btn-primary mx-2">Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoInput;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+    return (
+        <div>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+    )
+}
+
+export default TodoList;
