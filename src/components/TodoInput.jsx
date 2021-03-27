84bfe9d793ef6351d8fbe4943f0ecf413f887397
Fernando Oliveira <fernandoocf22@gmail.com>
2021-03-26 21:02:19 -0300

    done local todoList

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import {addTodo} from '../redux/actions';
+import {useDispatch} from "react-redux";
 
 function TodoInput() {
+    const [name, setName] = useState();
+    const dispatch = useDispatch();
     return (
         <div>
             <div className='row'>
-                <input type="text" className="col form-control"/>
-                <button className="btn btn-primary mx-2">Add</button>
+                <input
+                    type="text"
+                    className="col form-control"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}/>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => {
+                        dispatch(addTodo({id: Math.floor(Math.random() * 20), name: name}))
+                            setName('')
+                    }}>Add</button>
             </div>
         </div>
     )
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
+import {deleteTodo, updateTodo} from '../redux/actions';
 
-function TodoItem() {
+function TodoItem({todo}) {
+    const [editable, setEditable] = useState(false);
+    const [name, setName] = useState('');
+    const dispatch = useDispatch(todo.name);
     return (
         <div>
             <div className="row mx-2 align-items-center">
-                <div>#1</div>
+                <div>{todo.id}</div>
                 <div className="col">
-                    <h4>Todo Title</h4>
+                    {editable ?
+                        <input type="text"
+                               className="form-control"
+                               placeholder={todo.name}
+                               value={name}
+                               onChange={(e) => setName(e.target.value)} />
+                        : <h4>{todo.name}</h4>}
                 </div>
-                <button className="btn btn-primary m-2">Edit</button>
-                <button className="btn btn-danger m-2">Delete</button>
+                <button
+                    className="btn btn-primary m-2"
+                    onClick={() => {
+                        dispatch(updateTodo(
+                            {
+                                ...todo,
+                                name: name
+                            }
+                        ))
+                        if (editable) {
+                            setName(todo.name);
+                        }
+                        setEditable(!editable);
+                    }}>{editable ? "Update" : "Edit"}</button>
+                <button className="btn btn-danger m-2"  onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
             </div>
         </div>
     )
