e38d06fe11fb76fc777c40be8f0a6b164d8fbeaa
Fernando Oliveira <fernandoocf22@gmail.com>
2021-03-28 18:31:18 -0300

    adding tests file

---
+++ src/test/Todo.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import TodoInput from "../components/TodoInput";
+import {store} from "../redux/store";
+
+
+
+describe("Tests for TodoInput component", () => {
+    it("Should load a new task to the list", () => {
+
+    });
+
+    it("Should add a new task to the list", () => {
+
+    });
+
+    it("Should delete a new task to the list", () => {
+
+    });
+
+    it("Should update a new task to the list", () => {
+
+    });
+});
